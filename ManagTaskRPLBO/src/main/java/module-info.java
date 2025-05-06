6859b3554bd0e066eca66dd3970bae090655991b
mosessatriyo23 <143705697+mosessatriyo23@users.noreply.github.com>
2025-05-06 22:20:17 +0700

    Delete ManagTaskRPLBO/src/main/java directory

--- ManagTaskRPLBO/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.rplbo.ukdw.managtaskrplbo {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.rplbo.ukdw.managtaskrplbo to javafx.fxml;
-    exports com.rplbo.ukdw.managtaskrplbo;
-}
