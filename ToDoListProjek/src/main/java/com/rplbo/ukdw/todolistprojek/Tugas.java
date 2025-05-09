ad37c278b2b9a565216916bf8709c96d3b77f0e7
mosessatriyo23 <143705697+mosessatriyo23@users.noreply.github.com>
2025-05-09 19:47:59 +0700

    new

---
+++ ToDoListProjek/src/main/java/com/rplbo/ukdw/todolistprojek/Tugas.java
@@ -0,0 +1,10 @@
+package com.rplbo.ukdw.todolistprojek;
+
+public class Tugas {
+    private int id;
+    private String judul;
+    private String deskripsi;
+    private String tanggal;
+    private boolean prioritas;
+    private String kategori;
+}
